3a1df0d2b9d3e6c7d29b7ddee0d686d4f6969a48
yuvraj singh <yuvipaji222002@gmail.com>
2025-09-03 14:01:03 +0530

    update Ec2

--- src/Components/auth/Signup.jsx
+++ src/Components/auth/Signup.jsx
@@ -22,7 +22,7 @@ const Signup = () => {
 
     try {
       setLoading(true);
-      const res = await axios.post("18.117.108.0/signup", {
+      const res = await axios.post("18.117.108.0:3000/signup", {
         email: email,
         password: password,
         username: username,
